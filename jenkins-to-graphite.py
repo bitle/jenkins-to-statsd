9d6721004071eee853cc8feeeecaec9a639d2273
Sean Houghton <sean_houghton@playstation.sony.com>
2014-10-10 17:59:05 -0700

    Convert to statsd. Jobs is now called view. Add new per-job stats.

--- jenkins-to-graphite.py
+++ jenkins-to-graphite.py
@@ -20,11 +20,12 @@
 import base64
 import logging
 import optparse
-import os
 import sys
-import socket
 import time
 import urllib2
+import statsd
+from urlparse import urlparse
+from datetime import datetime
 
 try:
     # this should be available in any python 2.6 or newer
@@ -78,37 +79,6 @@ class JenkinsServer(object):
         return self.get_raw_data("%s/api/json" % url)
 
 
-class GraphiteServer(object):
-    def __init__(self, server, port, prefix):
-        self.server = server
-        self.port = int(port)
-        self.prefix = prefix.rstrip('.')
-
-        self.data = {}
-
-    def add_data(self, key, value):
-        self.data["%s.%s" % (self.prefix, key)] = value
-
-    def _data_as_msg(self):
-        msg = ""
-        now = time.time()
-        for (key, val) in self.data.items():
-            msg += "%s %s %s\n" % (key, val, now)
-        return msg
-
-    def send(self):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.server, self.port))
-            s.sendall(self._data_as_msg())
-#            print self._data_as_msg()
-            s.close()
-        except Exception, e:
-            logging.warn("Unable to send msg to graphite: %s" % (e,))
-            return False
-
-        return True
-
 
 def parse_args():
     parser = optparse.OptionParser()
@@ -123,12 +93,14 @@ def parse_args():
     parser.add_option("", "--jenkins-password",
                       help="Password for authenticating with jenkins")
 
-    parser.add_option("", "--jobs",
-                      help="Jobs view to monitor for success/failure")
+    parser.add_option("", "--view",
+                      help="View to monitor for success/failure")
     parser.add_option("", "--prefix", default="jenkins",
                       help="Graphite metric prefix")
     parser.add_option("", "--label", action="append", dest="labels",
                       help="Fetch stats applicable to this node label. Can bee applied multiple times for monitoring more labels.")
+    parser.add_option("", "--job", action="append", dest="individual_jobs",
+                      help="Fetch stats applicable to this job. Can bee applied multiple times for monitoring more jobs.")
 
     (opts, args) = parser.parse_args()
 
@@ -143,54 +115,77 @@ def main():
     opts = parse_args()
     jenkins = JenkinsServer(opts.jenkins_url, opts.jenkins_user,
                             opts.jenkins_password)
-    graphite = GraphiteServer(opts.graphite_server, opts.graphite_port,
-                              opts.prefix)
 
+    parsed_jenkins_url = urlparse(opts.jenkins_url)
+    prefix = opts.prefix + '.' + parsed_jenkins_url.netloc.replace('.', '-')
+    stats = statsd.StatsClient(opts.graphite_server, 8125, prefix=prefix)
+
+    print("Loading computers...")
     executor_info = jenkins.get_data("computer")
+
+    print("Loading queue...")
     queue_info = jenkins.get_data("queue")
+
+    print("Loading timeline...")
     build_info_min = jenkins.get_raw_data("view/All/timeline/data?min=%d&max=%d" % ((time.time() - 60) * 1000, time.time() * 1000))
     build_info_hour = jenkins.get_raw_data("view/All/timeline/data?min=%d&max=%d" % ((time.time() - 3600) * 1000, time.time() * 1000))
 
-    graphite.add_data("queue.size", len(queue_info.get("items", [])))
+    stats.gauge("queue.size", len(queue_info.get("items", [])))
 
-    graphite.add_data("builds.started_builds_last_minute", len(build_info_min.get("events", [])))
-    graphite.add_data("builds.started_builds_last_hour", len(build_info_hour.get("events", [])))
+    stats.gauge("builds.started_builds_last_minute", len(build_info_min.get("events", [])))
+    stats.gauge("builds.started_builds_last_hour", len(build_info_hour.get("events", [])))
 
-    graphite.add_data("executors.total", executor_info.get("totalExecutors", 0))
-    graphite.add_data("executors.busy", executor_info.get("busyExecutors", 0))
-    graphite.add_data("executors.free",
-                      executor_info.get("totalExecutors", 0) -
-                      executor_info.get("busyExecutors", 0))
+    stats.gauge("executors.total", executor_info.get("totalExecutors", 0))
+    stats.gauge("executors.busy", executor_info.get("busyExecutors", 0))
+    stats.gauge("executors.free",
+                   executor_info.get("totalExecutors", 0) -
+                   executor_info.get("busyExecutors", 0))
 
     nodes_total = executor_info.get("computer", [])
     nodes_offline = [j for j in nodes_total if j.get("offline")]
-    graphite.add_data("nodes.total", len(nodes_total))
-    graphite.add_data("nodes.offline", len(nodes_offline))
-    graphite.add_data("nodes.online", len(nodes_total) - len(nodes_offline))
+    stats.gauge("nodes.total", len(nodes_total))
+    stats.gauge("nodes.offline", len(nodes_offline))
+    stats.gauge("nodes.online", len(nodes_total) - len(nodes_offline))
 
     if opts.labels:
         for label in opts.labels:
             label_info = jenkins.get_data("label/%s" % label)
-            graphite.add_data("labels.%s.jobs.tiedJobs" % label, len(label_info.get("tiedJobs", [])))
-            graphite.add_data("labels.%s.nodes.total" % label, len(label_info.get("nodes", [])))
-            graphite.add_data("labels.%s.executors.total" % label, label_info.get("totalExecutors", 0))
-            graphite.add_data("labels.%s.executors.busy" % label, label_info.get("busyExecutors", 0))
-            graphite.add_data("labels.%s.executors.free" % label,
+            stats.gauge("labels.%s.jobs.tiedJobs" % label, len(label_info.get("tiedJobs", [])))
+            stats.gauge("labels.%s.nodes.total" % label, len(label_info.get("nodes", [])))
+            stats.gauge("labels.%s.executors.total" % label, label_info.get("totalExecutors", 0))
+            stats.gauge("labels.%s.executors.busy" % label, label_info.get("busyExecutors", 0))
+            stats.gauge("labels.%s.executors.free" % label,
                               label_info.get("totalExecutors", 0) -
                               label_info.get("busyExecutors", 0))
 
-    if opts.jobs:
-        builds_info = jenkins.get_data("/view/%s" % opts.jobs)
+    if opts.individual_jobs:
+        for job in opts.individual_jobs:
+            print("Loading information for job '%s'" % job)
+            in_queue = [item for item in queue_info.get("items", []) if item.get("task", {}).get("name", None) == job]
+            stats.gauge("jobs.%s.queue.size" % job, len(in_queue))
+            if in_queue:
+                longest_in_queue = min([datetime.fromtimestamp(item.get("inQueueSince", 1000*int(time.time()))/1000) for item in in_queue])
+                delay = datetime.now() - longest_in_queue
+                delay_seconds = delay.seconds + (delay.days * 86400)
+            else:
+                delay_seconds = 0
+            stats.timing("jobs.%s.queue.delay" % job, delay_seconds*1000)
+            last_successful = jenkins.get_data("job/%s/lastSuccessfulBuild" % job)
+            last_duration = last_successful.get('duration', None)
+            stats.timing("jobs.%s.duration" % job, last_duration)
+
+    if opts.view:
+        print("Loading information for view '%s'" % opts.view)
+        builds_info = jenkins.get_data("/view/%s" % opts.view)
         jobs = builds_info.get("jobs", [])
         ok = [j for j in jobs if j.get("color", 0) == "blue"]
         fail = [j for j in jobs if j.get("color", 0) == "red"]
         warn = [j for j in jobs if j.get("color", 0) == "yellow"]
-        graphite.add_data("jobs.total", len(jobs))
-        graphite.add_data("jobs.ok", len(ok))
-        graphite.add_data("jobs.fail", len(fail))
-        graphite.add_data("jobs.warn", len(warn))
-
-    graphite.send()
+        
+        stats.gauge("view.%s.total" % opts.view, len(jobs))
+        stats.gauge("view.%s.ok" % opts.view, len(ok))
+        stats.gauge("view.%s.fail" % opts.view, len(fail))
+        stats.gauge("view.%s.warn" % opts.view, len(warn))
 
 if __name__ == "__main__":
     main()
