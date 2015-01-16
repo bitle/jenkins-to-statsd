a0006ca6ba026a5f420c91715709f74fd19019b4
Damir Suleymanov <damir@cloudlock.com>
2015-01-16 13:52:30 -0500

    Change statsd to librato

--- jenkins-to-graphite.py
+++ jenkins-to-graphite.py
@@ -23,7 +23,7 @@ import optparse
 import sys
 import time
 import urllib2
-import statsd
+import librato
 from urlparse import urlparse
 from datetime import datetime
 import json
@@ -43,7 +43,7 @@ class JenkinsServer(object):
         if self._opener is None:
             opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
             if self.user or self.password:
-                opener.addheaders = [(("Authorization", "Basic " + base64.encodestring("%s:%s" % (self.user, self.password))))]
+                opener.addheaders = [("Authorization", "Basic " + base64.encodestring("%s:%s" % (self.user, self.password)))]
             urllib2.install_opener(opener)
             self._opener = opener
 
@@ -67,13 +67,12 @@ class JenkinsServer(object):
         return self.get_raw_data("%s/api/json" % url)
 
 
-
 def parse_args():
     parser = optparse.OptionParser()
-    parser.add_option("", "--statsd-server",
-                      help="Host name of the server running graphite")
-    parser.add_option("", "--statsd-port", type=int,
-                      default=8125)
+    parser.add_option("", "--librato-user",
+                      help="Librato username")
+    parser.add_option("", "--librato-token",
+                      help="Librato access token")
     parser.add_option("", "--jenkins-url",
                       help="Base url of your jenkins server (ex http://jenkins.example.com")
     parser.add_option("", "--jenkins-user",
@@ -92,8 +91,8 @@ def parse_args():
 
     (opts, args) = parser.parse_args()
 
-    if not opts.statsd_server or not opts.jenkins_url:
-        print >> sys.stderr, "Need to specify statsd server and jenkins url"
+    if not opts.jenkins_url:
+        print >> sys.stderr, "Need to specify jenkins url"
         sys.exit(1)
 
     return opts
@@ -106,7 +105,7 @@ def main():
 
     parsed_jenkins_url = urlparse(opts.jenkins_url)
     prefix = opts.prefix + '.' + parsed_jenkins_url.netloc.replace('.', '-')
-    stats = statsd.StatsClient(opts.statsd_server, opts.statsd_port, prefix=prefix)
+    api = librato.connect(opts.librato_user, opts.librato_token, base_path=prefix)
 
     print("Loading computers...")
     executor_info = jenkins.get_data("computer")
@@ -118,22 +117,22 @@ def main():
     build_info_min = jenkins.get_raw_data("view/All/timeline/data?min=%d&max=%d" % ((time.time() - 60) * 1000, time.time() * 1000))
     build_info_hour = jenkins.get_raw_data("view/All/timeline/data?min=%d&max=%d" % ((time.time() - 3600) * 1000, time.time() * 1000))
 
-    stats.gauge("queue.size", len(queue_info.get("items", [])))
+    api.submit("queue.size", len(queue_info.get("items", [])), type="gauge")
 
-    stats.gauge("builds.started_builds_last_minute", len(build_info_min.get("events", [])))
-    stats.gauge("builds.started_builds_last_hour", len(build_info_hour.get("events", [])))
+    api.submit("builds.started_builds_last_minute", len(build_info_min.get("events", [])))
+    api.submit("builds.started_builds_last_hour", len(build_info_hour.get("events", [])))
 
-    stats.gauge("executors.total", executor_info.get("totalExecutors", 0))
-    stats.gauge("executors.busy", executor_info.get("busyExecutors", 0))
-    stats.gauge("executors.free",
-                   executor_info.get("totalExecutors", 0) -
-                   executor_info.get("busyExecutors", 0))
+    api.submit("executors.total", executor_info.get("totalExecutors", 0))
+    api.submit("executors.busy", executor_info.get("busyExecutors", 0))
+    api.submit("executors.free",
+               executor_info.get("totalExecutors", 0) -
+               executor_info.get("busyExecutors", 0))
 
     nodes_total = executor_info.get("computer", [])
     nodes_offline = [j for j in nodes_total if j.get("offline")]
-    stats.gauge("nodes.total", len(nodes_total))
-    stats.gauge("nodes.offline", len(nodes_offline))
-    stats.gauge("nodes.online", len(nodes_total) - len(nodes_offline))
+    api.submit("nodes.total", len(nodes_total))
+    api.submit("nodes.offline", len(nodes_offline))
+    api.submit("nodes.online", len(nodes_total) - len(nodes_offline))
     node_names_offline = [n['displayName'] for n in nodes_offline]
 
     if opts.labels:
@@ -141,31 +140,31 @@ def main():
             print("Loading information for label '%s'" % label)
             label_info = jenkins.get_data("label/%s" % label)
             label_node_names = [n['nodeName'] for n in label_info.get('nodes', [])]
-            stats.gauge("labels.%s.jobs.tiedJobs" % label, len(label_info.get("tiedJobs", [])))
-            stats.gauge("labels.%s.nodes.total" % label, len(label_node_names))
-            stats.gauge("labels.%s.executors.total" % label, label_info.get("totalExecutors", 0))
-            stats.gauge("labels.%s.executors.busy" % label, label_info.get("busyExecutors", 0))
-            stats.gauge("labels.%s.executors.free" % label,
-                              label_info.get("totalExecutors", 0) -
-                              label_info.get("busyExecutors", 0))
+            api.submit("labels.%s.jobs.tiedJobs" % label, len(label_info.get("tiedJobs", [])))
+            api.submit("labels.%s.nodes.total" % label, len(label_node_names))
+            api.submit("labels.%s.executors.total" % label, label_info.get("totalExecutors", 0))
+            api.submit("labels.%s.executors.busy" % label, label_info.get("busyExecutors", 0))
+            api.submit("labels.%s.executors.free" % label,
+                       label_info.get("totalExecutors", 0) -
+                       label_info.get("busyExecutors", 0))
             label_offline_nodes = set(node_names_offline).intersection(set(label_node_names))
-            stats.gauge("labels.%s.executors.offline" % label, len(label_offline_nodes))
+            api.submit("labels.%s.executors.offline" % label, len(label_offline_nodes))
 
     if opts.individual_jobs:
         for job in opts.individual_jobs:
             print("Loading information for job '%s'" % job)
             in_queue = [item for item in queue_info.get("items", []) if item.get("task", {}).get("name", None) == job]
-            stats.gauge("jobs.%s.queue.size" % job, len(in_queue))
+            api.submit("jobs.%s.queue.size" % job, len(in_queue))
             if in_queue:
-                longest_in_queue = min([datetime.fromtimestamp(item.get("inQueueSince", 1000*int(time.time()))/1000) for item in in_queue])
+                longest_in_queue = min([datetime.fromtimestamp(item.get("inQueueSince", 1000 * int(time.time())) / 1000) for item in in_queue])
                 delay = datetime.now() - longest_in_queue
                 delay_seconds = delay.seconds + (delay.days * 86400)
             else:
                 delay_seconds = 0
-            stats.timing("jobs.%s.queue.delay" % job, delay_seconds*1000)
+            api.submit("jobs.%s.queue.delay" % job, delay_seconds*1000)
             last_successful = jenkins.get_data("job/%s/lastSuccessfulBuild" % job)
             last_duration = last_successful.get('duration', None)
-            stats.timing("jobs.%s.duration" % job, last_duration)
+            api.submit("jobs.%s.duration" % job, last_duration)
 
     if opts.view:
         print("Loading information for view '%s'" % opts.view)
@@ -175,10 +174,10 @@ def main():
         fail = [j for j in jobs if j.get("color", 0) == "red"]
         warn = [j for j in jobs if j.get("color", 0) == "yellow"]
         
-        stats.gauge("view.%s.total" % opts.view, len(jobs))
-        stats.gauge("view.%s.ok" % opts.view, len(ok))
-        stats.gauge("view.%s.fail" % opts.view, len(fail))
-        stats.gauge("view.%s.warn" % opts.view, len(warn))
+        api.submit("view.%s.total" % opts.view, len(jobs))
+        api.submit("view.%s.ok" % opts.view, len(ok))
+        api.submit("view.%s.fail" % opts.view, len(fail))
+        api.submit("view.%s.warn" % opts.view, len(warn))
 
 if __name__ == "__main__":
     try:
